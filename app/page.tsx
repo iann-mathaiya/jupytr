14c32e84f4b6bef20461a0e3295c086eeb73ee29
Ian Mathaiya <iannmathaiya@gmail.com>
2023-02-21 05:09:33 +0300

    add post component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+'use client'
+import CreatePost from "./components/AddPost"
+
 export default function Home() {
   return (
-    <main>
-       <h1 className="text-2xl font-bold">
-      Hello world!
-    </h1>
+    <main className="px-20 flex items-center justify-center">
+      {/* <h1 className="text-2xl font-bold">
+        Hello world!
+      </h1> */}
+      <CreatePost />
     </main>
   )
 }
