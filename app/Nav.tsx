1a26388173f0d2f57da52f11717400080f06898e
Ian Mathaiya <iannmathaiya@gmail.com>
2023-02-21 04:40:02 +0300

    sign out

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -5,7 +5,10 @@ import { supabase } from "@/lib/supabaseClient"
 
 export default async function Nav() {
 
-    const session = await supabase.auth.getSession()
+    const {data, error} = await supabase.auth.getSession()
+
+    console.log(data.session);
+    
 
     return (
         <div className="flex">
@@ -28,8 +31,9 @@ export default async function Nav() {
                 </div>
 
                 <ul className="flex items-center gap-6">
-                    { !session?.data && <Login /> }
-                    { session?.data &&  <Profile /> }
+                    { !data?.session === null && <Login /> || <Profile />  }
+                    {/* { data?.session &&  <Profile /> || "" } */}
+                    {/* {data?.session ? <Profile /> : <Login />} */}
                 </ul>
 
             </nav>
